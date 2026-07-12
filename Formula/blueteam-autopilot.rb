93ac837506f6e4e62a8aae73565d224b889998d6
cdavis-code <chris@cdavis.ca>
2026-07-12 10:51:01 -0400

    chore: prepare formula for v3.1.5

--- Formula/blueteam-autopilot.rb
+++ Formula/blueteam-autopilot.rb
@@ -3,7 +3,7 @@ class BlueteamAutopilot < Formula
 
   desc "SecOps agent powered by Qwen Cloud + ConnectOnion for multi-cloud security operations"
   homepage "https://github.com/cdavis-code/blueteam-autopilot"
-  url "https://github.com/cdavis-code/blueteam-autopilot/archive/refs/tags/v3.1.4.tar.gz"
+  url "https://github.com/cdavis-code/blueteam-autopilot/archive/refs/tags/v3.1.5.tar.gz"
   sha256 "71f8b79712ee789ca51f0c582947bc05fc231ecd0c70ec806c41627c15ab1826"
   license "MIT"
 
